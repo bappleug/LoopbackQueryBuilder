5c8cd1e1b073d88b7b5def2c2e98fe8e66b593a7
RayMin <rmin@thoughtworks.com>
2020-04-29 11:09:40 +0800

    test json array

--- src/test/java/tw/loopbackquery/newimpl/JsonMapTest.java
+++ src/test/java/tw/loopbackquery/newimpl/JsonMapTest.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
@@ -54,5 +55,30 @@ class JsonMapTest {
         final String json = objectMapper.writeValueAsString(map);
         assertThatJson(json).isEqualTo("{key: {nested: \"value\"}}");
     }
+
+    @Test
+    void should_return_json_object_with_primitive_array() throws JsonProcessingException {
+        Map<String, Object> map = new HashMap<>();
+        map.put("key", List.of(1, 2, 3));
+        final String json = objectMapper.writeValueAsString(map);
+        assertThatJson(json).isEqualTo("{key: [1, 2, 3]}");
+    }
+
+    @Test
+    void should_return_json_object_with_json_array_from_list_of_object() throws JsonProcessingException {
+        Map<String, Object> map = new HashMap<>();
+        map.put("key", List.of(Map.of("nested1", 1), Map.of("nested2", 2), Map.of("nested3", 3)));
+        final String json = objectMapper.writeValueAsString(map);
+        assertThatJson(json).isEqualTo("{key: [{nested1:1}, {nested2:2}, {nested3:3}]}");
+    }
+
+    @Test
+    void should_return_json_object_with_json_array_from_array_of_object() throws JsonProcessingException {
+        Map<String, Object> map = new HashMap<>();
+        map.put("key", new Map[]{Map.of("nested1", 1), Map.of("nested2", 2), Map.of("nested3", 3)});
+        final String json = objectMapper.writeValueAsString(map);
+        assertThatJson(json).isEqualTo("{key: [{nested1:1}, {nested2:2}, {nested3:3}]}");
+    }
+
 
 }
